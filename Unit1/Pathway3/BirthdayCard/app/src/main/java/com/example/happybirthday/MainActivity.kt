6ae0e73bbc6fdf5c0d99f3f4070e0e88e18597e5
sub101 <ssubbin10@gmail.com>
2023-12-01 23:44:30 +0900

    practice compose

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -18,22 +18,43 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            HappyBirthdayTheme() {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    BirthdayGreetingWithImage(
+                        message = getString(R.string.happy_birthday_txt),
+                        from = getString(
+                            R.string.from_sb,
+                        ),
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -56,18 +77,36 @@ fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier =
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End),
+                .align(alignment = Alignment.CenterHorizontally),
         )
     }
 }
 
 // 5. Box 레이아웃 추가
 @Composable
-fun BirthdayGreetingWithImage(message: String, from: String) { }
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val img = painterResource(id = R.drawable.androidparty)
+
+    Box {
+        Image(
+            painter = img,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F,
+        )
+        BirthdayGreetingWithText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+        )
+    }
+}
 
 // 4. 이미지 컴포저블 추가
 @Preview(showBackground = true)
 @Composable
 private fun BirthdayCardPreview() {
-    BirthdayGreetingWithText("HBD CH !", "From SB")
+    BirthdayGreetingWithImage(stringResource(R.string.happy_birthday_txt), stringResource(R.string.from_sb))
 }
